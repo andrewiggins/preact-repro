a9c661667ca49a9ee4197ec90785f5657e77dadd
Andre Wiggins <andrewiggins@live.com>
2019-05-20 14:34:07 -0700

    Rename dom.js to backward-loop.js

--- src/diffArray/index.js
+++ src/diffArray/index.js
@@ -1,6 +1,6 @@
 // import "./arrays";
 import { runTests } from "./runTest";
-import { diffChildren } from "./dom";
+import { diffChildren } from "./backward-loop";
 import { preactDiffChildren } from "./preact";
 
 if (location.href.indexOf('preact') != -1) {
