12ed53201df66f7b0b8db4a7c4d1e71ea734693e
Andre Wiggins <andrewiggins@live.com>
2020-07-28 14:10:59 -0700

    Upgrade dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,32 @@
 import path from "path";
-import buble from "rollup-plugin-buble";
-import alias from "rollup-plugin-alias";
-import nodeResolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
+import alias from "@rollup/plugin-alias";
+import sucrase from "@rollup/plugin-sucrase";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 
 const p = (...paths) => path.join(__dirname, ...paths);
 
 export default {
 	input: "./src/index.js",
+	preserveEntrySignatures: false, // Per rollup warning
 	output: {
 		dir: "./dist",
 		format: "esm",
-		chunkFileNames: "[name].js" // Keep consistent names for easier debugging
+		chunkFileNames: "[name].js", // Keep consistent names for easier debugging
 	},
 	plugins: [
 		nodeResolve(),
 		commonjs(),
-		buble({
-			jsx: 'createElement'
+		sucrase({
+			transforms: ["jsx"],
+			jsxPragma: "createElement",
+			jsxFragPragma: "Fragment",
 		}),
 		alias({
-			"preact/hooks": p("lib/preact/hooks.js"),
-			preact: p("lib/preact/index.js")
-		})
-	]
+			entries: {
+				"preact/hooks": p("lib/preact/hooks.js"),
+				preact: p("lib/preact/index.js"),
+			},
+		}),
+	],
 };
